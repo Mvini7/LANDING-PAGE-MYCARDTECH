2b51146c8986d4c69f70537e11d92a1d74741b86
VINICIUS RODRIGUES <158781792+Mvini7@users.noreply.github.com>
2024-12-17 20:56:58 -0300

    responsividade do sobre e inicio

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+const btnMenu = document.getElementById('#menu');
+const menu = document.querySelector('ul')
+// Adiciona o evento de clique ao botão de menu
+function menuu(){
+    menu.classList.toggle('active');
+};
