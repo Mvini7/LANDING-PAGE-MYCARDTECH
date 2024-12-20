30682c22302268db8991ca7989596e796024be98
VINICIUS RODRIGUES <158781792+Mvini7@users.noreply.github.com>
2024-12-20 14:00:21 -0300

    criação dos produtos

---
+++ js/produtos.js
@@ -0,0 +1,17 @@
+const checkout1 = document.getElementById('checkout1')
+
+checkout1.addEventListener('click', function() {
+    window.location.href = 'https://mycardtech.pay.yampi.com.br/r/JVDNYWULWD'
+})
+
+const checkout2 = document.getElementById('checkout2')
+
+checkout1.addEventListener('click', function() {
+        window.location.href = 'https://mycardtech.pay.yampi.com.br/checkout?skipToCheckout=1&tokenReference=CI0N2UZY4H'
+})
+
+const checkout3 = document.getElementById('checkout3')
+
+checkout1.addEventListener('click', function() {
+        window.location.href = 'https://mycardtech.pay.yampi.com.br/r/EAUSNQJMFY'
+})
