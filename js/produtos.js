81ccdd20ef3c83ebf2772222da34a1445f158090
VINICIUS RODRIGUES <158781792+Mvini7@users.noreply.github.com>
2024-12-26 17:56:54 -0300

    reconfiguração dos checkouts

--- js/produtos.js
+++ js/produtos.js
@@ -7,11 +7,17 @@ checkout1.addEventListener('click', function() {
 const checkout2 = document.getElementById('checkout2')
 
 checkout2.addEventListener('click', function() {
-        window.location.href = 'https://mycardtech.pay.yampi.com.br/checkout?skipToCheckout=1&tokenReference=CI0N2UZY4H'
+        window.location.href = 'https://mycardtech.pay.yampi.com.br/r/JVDNYWULWD'
 })
 
 const checkout3 = document.getElementById('checkout3')
 
 checkout3.addEventListener('click', function() {
+        window.location.href = 'https://mycardtech.pay.yampi.com.br/r/EAUSNQJMFY'
+})
+
+const checkout3_2 = document.getElementById('checkout3_2')
+
+checkout3_2.addEventListener('click', function() {
         window.location.href = 'https://mycardtech.pay.yampi.com.br/r/EAUSNQJMFY'
 })
