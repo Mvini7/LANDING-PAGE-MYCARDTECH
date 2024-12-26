27b5aa93785cc6c72dd85a9fa3c4cc70185a10fc
VINICIUS RODRIGUES <158781792+Mvini7@users.noreply.github.com>
2024-12-26 17:31:55 -0300

    mundança nos checkouts

--- js/produtos.js
+++ js/produtos.js
@@ -6,12 +6,12 @@ checkout1.addEventListener('click', function() {
 
 const checkout2 = document.getElementById('checkout2')
 
-checkout1.addEventListener('click', function() {
+checkout2.addEventListener('click', function() {
         window.location.href = 'https://mycardtech.pay.yampi.com.br/checkout?skipToCheckout=1&tokenReference=CI0N2UZY4H'
 })
 
 const checkout3 = document.getElementById('checkout3')
 
-checkout1.addEventListener('click', function() {
+checkout3.addEventListener('click', function() {
         window.location.href = 'https://mycardtech.pay.yampi.com.br/r/EAUSNQJMFY'
 })
