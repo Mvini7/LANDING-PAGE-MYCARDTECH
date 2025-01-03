ec92b2563dcf5d9845c16d3cf0f3cc7fe1c32a68
VINICIUS RODRIGUES <158781792+Mvini7@users.noreply.github.com>
2025-01-02 21:03:44 -0300

    ajustes finais

--- js/produtos.js
+++ js/produtos.js
@@ -7,7 +7,7 @@ checkout1.addEventListener('click', function() {
 const checkout2 = document.getElementById('checkout2')
 
 checkout2.addEventListener('click', function() {
-        window.location.href = 'https://mycardtech.pay.yampi.com.br/r/JVDNYWULWD'
+        window.location.href = 'https://mycardtech.pay.yampi.com.br/r/CI0N2UZY4H'
 })
 
 const checkout3 = document.getElementById('checkout3')
