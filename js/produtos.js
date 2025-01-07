800b626091106093a686df693e0e2df14fcf1a10
VINICIUS RODRIGUES <158781792+Mvini7@users.noreply.github.com>
2025-01-07 20:51:52 -0300

    adicionado o seletor de cores do produto 1

--- js/produtos.js
+++ js/produtos.js
@@ -1,9 +1,3 @@
-const checkout1 = document.getElementById('checkout1')
-
-checkout1.addEventListener('click', function() {
-    window.location.href = 'https://mycardtech.pay.yampi.com.br/r/JVDNYWULWD'
-})
-
 const checkout2 = document.getElementById('checkout2')
 
 checkout2.addEventListener('click', function() {
@@ -15,3 +9,27 @@ const checkout3 = document.getElementById('checkout3')
 checkout3.addEventListener('click', function() {
         window.location.href = 'https://mycardtech.pay.yampi.com.br/r/EAUSNQJMFY'
 })
+
+function changeColor(color) {
+        const productImage = document.getElementById("product-image");
+        const checkoutLink = document.getElementById("checkout-link");
+
+        // Remover a borda de todas as cores
+        document.getElementById("color-black").classList.remove("selected");
+        document.getElementById("color-blue").classList.remove("selected");
+        document.getElementById("color-red").classList.remove("selected");
+
+        // Adicionar a borda preta à cor selecionada
+        document.getElementById(`color-${color}`).classList.add("selected");
+
+        if (color === 'black') {
+                productImage.src = "./img/card-preto.png";
+                checkoutLink.href = "https://mycardtech.pay.yampi.com.br/r/AWGWNRU2U9";
+        } else if (color === 'blue') {
+                productImage.src = "./img/card-azul.png";
+                checkoutLink.href = "https://mycardtech.pay.yampi.com.br/r/EKTNYO5IIH";
+        } else if (color === 'red') {
+                productImage.src = "./img/card-vermelho.png";
+                checkoutLink.href = "https://mycardtech.pay.yampi.com.br/r/DSZDITEK3K";
+        }
+}
